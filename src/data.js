826e22bcd8381b2e17c1afb9f108957aa3480c03
Maximilian Liesegang <max@liesegang.io>
2024-12-23 14:07:13 +0100

    Initial commit

--- src/data.js
+++ src/data.js
@@ -1,21 +1,7 @@
-sum_bike
-    :
-    1
-sum_car_truck_bus
-    :
-    0
-sum_injured_bike
-    :
-    1
-sum_injured_ped
-    :
-    0
-sum_other_modes
-    :
-    1
-sum_ped
-    :
-    0
-sum_severely_injured_bike
-    :
-    0
+sum_bike: 1;
+sum_car_truck_bus: 0;
+sum_injured_bike: 1;
+sum_injured_ped: 0;
+sum_other_modes: 1;
+sum_ped: 0;
+sum_severely_injured_bike: 0;
